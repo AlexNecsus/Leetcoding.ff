9a5b8ec3eac379b2a5d1e68d1baf792b3bfd7f6f
Obukhov Amin <amin.necsus@gmail.com>
2026-08-05 18:14:15 +0300

    Time: 11 ms (56.41%) | Memory: 14.3 MB (57.52%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
-        freq is all we need , and there's also heap
-        1. we count freaquency, 2. change val with keys , 3. we go through frequeny in reverse order.
+        heap 
         """
-        count = collections.defaultdict(int) 
-        freq = [[] for i in range(len(nums) + 1)]
+        import heapq
+        from collections import Counter
 
-        for n in nums:
-            count[n] += 1
+        count = Counter(nums)
+        heap = []
+
         for n, c in count.items():
-            freq[c].append(n)
-        res = []
-        for i in range(len(freq)-1, -1, -1):
-            for f in freq[i]:
-                res.append(f)
-                if len(res) == k:
-                    return res
+            if len(heap) < k:
+                heapq.heappush(heap, (c, n))
+            else:
+                heapq.heappushpop(heap, (c, n))
+        return [h[1] for h in heap]
