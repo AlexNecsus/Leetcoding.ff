3996350166d58f7ad4cc5d34468544d42e03099d
Obukhov Amin <amin.necsus@gmail.com>
2026-08-05 20:10:26 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (70.42%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -3,21 +3,19 @@ class Solution(object):
         """
         so we need to find corelation between max and min values while executing
         binary search algo
-
+        but our guy says that we dont really have to look for a pivot.
         """
-        # if nums == []: return nums
-
-        # Min = nums[0]
-        # l, r = 0, len(nums) - 1 
-        # while l < r:
-        #     m = (l + r) // 2
-        #     # if nums[] 
-
-        #     if nums[m] < nums[l]:
-        #         r = m - 1                
-        #     else:
-        #         Min = min(nums[m], Min)
-        #         l = m + 1
-        # return Min
-
-        return sorted(nums)[0]
+        Min = nums[0]
+        l, r = 0, len(nums) - 1 
+        while l < r:
+            m = (l + r) // 2
+            if nums[l] < nums[r]:
+                Min = min(nums[l], Min) 
+                break
+            Min = min(nums[m], Min)
+            if nums[m] < nums[l]:
+                r = m - 1                
+            else:
+                Min = min(nums[r], Min)
+                l = m + 1
+        return Min
