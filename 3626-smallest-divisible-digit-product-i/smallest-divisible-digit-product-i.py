441b635ef0cceb0d1333b7386f507b282f0527b7
Obukhov Amin <amin.necsus@gmail.com>
2026-08-06 23:25:57 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (1.33%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
@@ -0,0 +1,26 @@
+import math
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        digit product eh
+        """
+        def dpn(n):
+            n = abs(n)
+            if n == 0:
+                return 0
+                
+            product = 1
+            while n > 0:
+                digit = n % 10  # Extract the last digit
+                if digit == 0:
+                    return 0    # Early exit optimization
+                product *= digit
+                n //= 10
+            return product
+        dp = dpn(n)
+        while dp % t != 0:
+            if dp == 0 and t % 2 == 0:
+                return n
+            n += 1
+            dp = dpn(n)
+        return n
