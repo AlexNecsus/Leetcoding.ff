17ff530f8a84c23b6b2f3a1da568b82e0a960b46
Obukhov Amin <amin.necsus@gmail.com>
2026-08-07 17:32:21 +0300

    Time: 167 ms (51.20%) | Memory: 13.4 MB (48.80%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,24 +1,19 @@
 class Solution(object):
     def minEatingSpeed(self, piles, h):
-        """
-        [3,6,7,11] max = 11, len = 4 -> range(4-11) -> mid(k) = 8, ceil(pile / k) -> (3.. / k) -> 1, 1, 1, 2 => 5 because this value isnt minimal nearset to hour we have to go either high or low, we choose low in our case
-         max and len(piles)
-        """
-        
-        def can_finish(k):
-            time = 0
+        # upgrade result to least possible time
+        # math.ceil for division rounding 
+        # if hours <= h 
+        # k =? 1 - max(piles)
+        res = [0]
+        l, r = 1, max(piles)
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
             for p in piles:
-                time += (p + k - 1) // k
-                if time > h:
-                    return False
-            return True
-
-        low, high = 1, max(piles)
-        
-        while low < high: 
-            k = (high + low) // 2
-            if can_finish(k):
-                high = k
+                hours += ((p + k - 1)// k)
+            if hours <= h:
+                res = k
+                r = k - 1
             else:
-                low = k + 1
-        return low
+                l = k + 1
+        return res
