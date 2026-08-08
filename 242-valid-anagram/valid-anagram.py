9f17734f2e0760880ac1b2696b7ca9f13f80e49e
Obukhov Amin <amin.necsus@gmail.com>
2026-08-08 07:21:26 +0300

    Time: 16 ms (25.43%) | Memory: 19.4 MB (46.28%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # either we can count using dictionary or use other shortcuts
+        """
+        we could set counter and decrement values of other values and if any value becomes negative or unknown letter come out then False
+        """
+        if len(t) != len(s): return False 
+        count = collections.defaultdict(int)
+        for l in s:
+            count[l] += 1
+        
+        for l in t:
+            count[l] -= 1
+            if count[l] < 0 or l not in count:
+                return False
+        return True # [l == 0 for l in count]
